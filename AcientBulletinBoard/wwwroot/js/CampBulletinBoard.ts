87a572f1cf79f3918ce8b05b1b568eb70fb155d9
Rockefeller <kenny71223@gmail.com>
2020-09-14 09:33:38 +0800

    CampBulletinBard Competion

--- AcientBulletinBoard/wwwroot/js/CampBulletinBoard.ts
+++ AcientBulletinBoard/wwwroot/js/CampBulletinBoard.ts
@@ -1,7 +1,25 @@
 ﻿$(document).ready(function () {
     $("#btnSubmit").click(function () {
         let data: object = new Object();
-        data["target"] = "PublicBulletinBoard";
+        var target: string;
+        switch ($("#InpCamp").val()) {
+            case "Wei":
+                target = "Wei";
+                break;
+            case "Shu":
+                target = "Shu";
+                break;
+            case "Wu":
+                target = "Wu";
+                break;
+            case "Foreign":
+                target = "Foreign";
+                break;
+            case "God":
+                target = "God";
+                break;
+        }
+        data["target"] = target + "CampBulletinBoard";
         data["name"] = $("#inpName").val();
         data["comment"] = $("#inpComment").val();
         $.post("/CommentSubmit", data)
